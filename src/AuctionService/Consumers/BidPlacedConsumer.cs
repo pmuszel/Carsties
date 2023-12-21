93fc50477d5679261ab54cef2ea18fbfa88474c4
Piotr Muszel <piotr.muszel@supremis.pl>
2023-12-21 12:40:03 +0100

    End of section 6

---
+++ src/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers
+{
+    public class BidPlacedConsumer : IConsumer<BidPlaced>
+    {
+        private readonly AuctionDBContext _dbContext;
+
+        public BidPlacedConsumer(AuctionDBContext dBContext)
+        {
+            _dbContext = dBContext;
+        }
+        public async Task Consume(ConsumeContext<BidPlaced> context)
+        {
+            Console.WriteLine("--> Consuming bid placed");
+            var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+            if(auction.CurrentHighBid == null 
+                || context.Message.BidStatus.Contains("Accepted") && context.Message.Amount > auction.CurrentHighBid)
+                {
+                    auction.CurrentHighBid = context.Message.Amount;
+                    await _dbContext.SaveChangesAsync();
+                }
+        }
+    }
+}
---
+++ src/SearchService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers
+{
+    public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+    {
+        public async Task Consume(ConsumeContext<AuctionFinished> context)
+        {
+            System.Console.WriteLine("--> Consuming auction finished");
+
+            var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+            if(context.Message.ItemSold)
+            {
+                auction.Winner = context.Message.Winner;
+                auction.SoldAmount = context.Message.Amount ?? 0;
+            }
+
+            auction.Status = "Finished";
+
+            await auction.SaveAsync();
+        }
+    }
+}
