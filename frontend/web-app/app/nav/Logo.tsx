c349969f3f48d1572ff5f0cf64b5a792a922acbc
Piotr Muszel <piotr.muszel@supremis.pl>
2024-02-01 11:35:29 +0100

    End of section 9

--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -1,14 +1,22 @@
 'use client'
 
 import { useParamsStore } from '@/hooks/useParamsStore';
+import { usePathname, useRouter } from 'next/navigation';
 import React from 'react'
 import { IoCarSportSharp } from 'react-icons/io5'
 
 export default function Logo() {
-    const reset = useParamsStore(state => state.reset);
+  const router = useRouter();
+  const pathname = usePathname();
+  const reset = useParamsStore(state => state.reset);
+
+  function doReset() {
+    if(pathname !== '/') router.push('/');
+    reset();
+  }
     
   return (
-        <div className='flex items-center gap-2 text-3xl font-semibold text-red-500 cursor-pointer' onClick={reset}>
+        <div className='flex items-center gap-2 text-3xl font-semibold text-red-500 cursor-pointer' onClick={doReset}>
                 <IoCarSportSharp size={34}/>
                 <div>Carsties Auctions</div>
         </div>
--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import {IoCarSportSharp} from 'react-icons/io5'
 import Search from './Search';
-import { useParamsStore } from '@/hooks/useParamsStore';
 import Logo from './Logo';
+import LoginButton from './LoginButton';
+import { getCurrentUser } from '../actions/authActions';
+import UserActions from './UserActions';
 
-export default function Navbar() {
+export default async function Navbar() {
  
+  const user = await getCurrentUser();
+
   return (
     <header className='sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md'>
         <Logo />
         <Search />
-        <div>Login</div>
+        {user ? (<UserActions user={user} />)
+         : (<LoginButton />)}
+        
     </header>
   )
 }
